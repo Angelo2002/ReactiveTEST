4da6ae08f04ce8e7acd0fded50368f2cfcb5cf2c
Angelo <angelo200284@gmail.com>
2024-10-02 00:44:39 -0600

    Intial Commit

---
+++ src/test/java/org/una/programmingiio/academichub/AcademichubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.programmingiio.academichub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademichubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
